53b9bbe95605dc9f1294736fe89a87cd36634c30
Mihail Kostov <kostov.mihail321@gmail.com>
2023-02-24 14:26:17 +0200

    Commit

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { combineReducers } from 'redux';
+import { legacy_createStore as createStore} from 'redux'
+import amountReducer from '../features/amount';
+import goodsReducer from '../features/goods';
+import positionReducer from '../features/position';
+
+const reducer = combineReducers({
+  amount: amountReducer,
+  goods: goodsReducer,
+  position: positionReducer,
+});
+
+const store = createStore(reducer, composeWithDevTools());
+
+export type RootState = ReturnType<typeof store.getState>
+
+export default store;
